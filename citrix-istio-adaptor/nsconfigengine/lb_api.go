101a016154fc998a3fb948613f446cc97f4ac0e1
Ajeeta <ajeeta@Ajeetas-MacBook-Pro.local>
2020-10-09 17:41:22 +0530

    keep old citrix-istio-adaptor in repo

---
+++ citrix-istio-adaptor/nsconfigengine/lb_api.go
@@ -0,0 +1,251 @@
+/*
+Copyright 2019 Citrix Systems, Inc
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+    http://www.apache.org/licenses/LICENSE-2.0
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package nsconfigengine
+
+import (
+	"fmt"
+	"github.com/chiradeep/go-nitro/config/basic"
+	"github.com/chiradeep/go-nitro/config/lb"
+	"github.com/chiradeep/go-nitro/config/ns"
+	"github.com/chiradeep/go-nitro/netscaler"
+	"log"
+	"net"
+	"strings"
+)
+
+// LBMonitor specifies attributes associated with Outlier Detection feature
+type LBMonitor struct {
+	Retries       int    // consecutive[Gateway]Errors of Istio's Outlier Detection (OD)
+	Interval      int    // interval of Istio's OD
+	IntervalUnits string // Units of Interval field.
+	DownTime      int    // baseEjectionTime of Istio's OD
+	DownTimeUnits string // Units of DownTime
+}
+
+// LBApi specifies the attributes associated with a load balancng entity on the Citrix-ADC
+type LBApi struct {
+	Name                      string
+	FrontendServiceType       string
+	LbMethod                  string
+	BackendServiceType        string
+	MaxConnections            int
+	MaxHTTP2ConcurrentStreams int
+	MaxRequestsPerConnection  int
+	NetprofileName            string
+	BackendTLS                []SSLSpec
+	LbMonitorObj              *LBMonitor
+}
+
+type timeUnit int
+
+// MSEC, SEC and MIN represents milliseconds, seconds and minutes.
+const (
+	MSEC timeUnit = iota
+	SEC
+	MIN
+	maxInterval     = 20940
+	maxDownTime     = 20939
+	defaultInterval = 5 // in seconds
+	defaultDownTime = 30
+	defaultRetries  = 1
+)
+
+func (t timeUnit) String() string {
+	return [...]string{"MSEC", "SEC", "MIN"}[t]
+}
+
+func (t timeUnit) ToNextLevel() int {
+	return [...]int{1000, 60, 60}[t]
+}
+
+// getLbMonName function gives the shortened name for lb monitor if entityName length is more than 56 chars
+// LB monitor can max be 63 chars long!
+func getLbMonName(entityName string) string {
+	name := entityName + "_lbmon"
+	if len(name) > 63 {
+		s := strings.Split(name, "_")
+		name = strings.Join([]string{s[2], s[3], s[4], "lbmon"}, "_")
+	}
+	return name
+}
+
+// convertTimeUnits function converts msec to sec or min if the value is more than maxLimit.
+// inUnit should not be minutes (MIN)
+func convertTimeUnits(inTime int, inUnit string, maxLimit int, defaultValue int) (int, string) {
+	var outTime int
+	var outUnit timeUnit
+
+	if inTime == 0 || inUnit == "" {
+		return defaultValue, "SEC" // default value in always seconds
+	}
+
+	var units = map[string]timeUnit{
+		"MSEC": MSEC,
+		"SEC":  SEC,
+		"MIN":  MIN,
+	}
+
+	inUnitIdx := units[inUnit]
+	for outUnit, outTime = inUnitIdx, inTime; outUnit <= MIN; outUnit++ {
+		if outTime > maxLimit && outUnit != MIN {
+			outTime = outTime / outUnit.ToNextLevel()
+		} else {
+			if outTime > maxLimit { // At this stage, it means unit is definitely MIN
+				outTime = maxLimit
+			}
+			break
+		}
+	}
+	return outTime, outUnit.String()
+}
+
+// NewLBApi returns a new LBApi object
+func NewLBApi(name string, frontendServiceType string, backendServiceType string, lbMethod string) *LBApi {
+	lbObj := new(LBApi)
+	lbObj.Name = name
+	lbObj.FrontendServiceType = frontendServiceType
+	lbObj.BackendServiceType = backendServiceType
+	lbObj.LbMethod = lbMethod
+	return lbObj
+}
+
+// Add method adds/updates an LB vserver and associated servicegroup on Citrix-ADC.
+// It also adds httpprofile and http-inline monitor if needed.
+func (lbObj *LBApi) Add(client *netscaler.NitroClient) error {
+	log.Printf("[TRACE] LBApi add: %v", lbObj)
+	confErr := newNitroError()
+	confErr.updateError(doNitro(client, nitroConfig{netscaler.Lbvserver.Type(), lbObj.Name, lb.Lbvserver{Name: lbObj.Name, Servicetype: lbObj.FrontendServiceType, Lbmethod: lbObj.LbMethod}, "add"}, nil, nil))
+	httpProfileName := "nshttp_default_profile"
+	if lbObj.MaxHTTP2ConcurrentStreams != 0 {
+		httpProfileName = "nshttp_profile_" + fmt.Sprint(lbObj.MaxHTTP2ConcurrentStreams)
+		confErr.updateError(doNitro(client, nitroConfig{netscaler.Nshttpprofile.Type(), httpProfileName, ns.Nshttpprofile{Name: httpProfileName, Http2: "ENABLED", Http2maxconcurrentstreams: lbObj.MaxHTTP2ConcurrentStreams}, "add"}, nil, nil))
+	}
+	sg := map[string]interface{}{"servicegroupname": lbObj.Name, "servicetype": lbObj.BackendServiceType, "maxclient": lbObj.MaxConnections, "maxreq": lbObj.MaxRequestsPerConnection, "usip": "NO"}
+	if lbObj.BackendServiceType == "HTTP" {
+		sg["httpprofilename"] = httpProfileName
+	}
+	if lbObj.NetprofileName != "" {
+		sg["netprofile"] = lbObj.NetprofileName
+	}
+	//TODO copy all servicegroup members before deleting and readding with new type
+	confErr.updateError(doNitro(client, nitroConfig{"servicegroup", lbObj.Name, sg, "add"}, nil, []nitroConfig{{"servicegroup", lbObj.Name, nil, "delete"}, {"servicegroup", lbObj.Name, sg, "add"}}))
+	confErr.updateError(doNitro(client, nitroConfig{netscaler.Lbvserver_servicegroup_binding.Type(), lbObj.Name, lb.Lbvserverservicegroupbinding{Name: lbObj.Name, Servicegroupname: lbObj.Name}, "add"}, []string{"Resource already exists"}, nil))
+	if lbObj.BackendServiceType == "SSL" || lbObj.BackendServiceType == "SSL_TCP" {
+		addSSLServiceGroup(client, lbObj.Name, lbObj.BackendTLS, confErr)
+	}
+	// Creating LB Monitor for outlier detection purpose
+	lbMonName := getLbMonName(lbObj.Name)
+	if lbObj.LbMonitorObj == nil {
+		// Remove HTTP-Inline LB monitor bound to servicegroup if outlier detection field is not passed in cluster resource
+		confErr.updateError(doNitro(client, nitroConfig{netscaler.Servicegroup_lbmonitor_binding.Type(), lbObj.Name, map[string]string{"servicegroupname": lbObj.Name, "monitor_name": lbMonName}, "delete"}, []string{"No such resource"}, nil))
+		confErr.updateError(doNitro(client, nitroConfig{netscaler.Lbmonitor.Type(), lbMonName, map[string]string{"monitorname": lbMonName, "type": "HTTP-INLINE"}, "delete"}, []string{"No such resource"}, nil))
+	} else { // Adds lb monitor, and bind it to the servicegroup
+		if lbObj.LbMonitorObj.Retries == 0 {
+			lbObj.LbMonitorObj.Retries = defaultRetries
+		}
+		// Citrix ADC can have max value of 20940 and 20939 for Interval and Downtime resp. but it can accept various types for duration.
+		// Convert the time to next level of unit in case time-value is more than max-allowed value.
+		lbObj.LbMonitorObj.Interval, lbObj.LbMonitorObj.IntervalUnits = convertTimeUnits(lbObj.LbMonitorObj.Interval, lbObj.LbMonitorObj.IntervalUnits, maxInterval, defaultInterval)
+		lbObj.LbMonitorObj.DownTime, lbObj.LbMonitorObj.DownTimeUnits = convertTimeUnits(lbObj.LbMonitorObj.DownTime, lbObj.LbMonitorObj.DownTimeUnits, maxDownTime, defaultDownTime)
+		confErr.updateError(doNitro(client, nitroConfig{netscaler.Lbmonitor.Type(), lbMonName, lb.Lbmonitor{Monitorname: lbMonName, Type: "HTTP-INLINE", Action: "DOWN", Respcode: []int{200}, Httprequest: "HEAD /", Retries: lbObj.LbMonitorObj.Retries, Interval: lbObj.LbMonitorObj.Interval, Units2: lbObj.LbMonitorObj.IntervalUnits, Downtime: lbObj.LbMonitorObj.DownTime, Units3: lbObj.LbMonitorObj.DownTimeUnits}, "add"}, []string{"Resource already exists"}, nil))
+		confErr.updateError(doNitro(client, nitroConfig{netscaler.Servicegroup_lbmonitor_binding.Type(), lbObj.Name, basic.Servicegrouplbmonitorbinding{Servicegroupname: lbObj.Name, Monitorname: lbMonName}, "add"}, []string{"The monitor is already bound to the service"}, nil))
+	}
+	return confErr.getError()
+}
+
+// Delete method deletes an LB vserver and associated servicegroup
+func (lbObj *LBApi) Delete(client *netscaler.NitroClient) error {
+	log.Printf("[TRACE] LBApi delete: %v", lbObj)
+	confErr := newNitroError()
+	confErr.updateError(doNitro(client, nitroConfig{netscaler.Lbvserver.Type(), lbObj.Name, nil, "delete"}, nil,
+		[]nitroConfig{{netscaler.Lbvserver.Type(), lbObj.Name, lb.Lbvserver{Name: lbObj.Name, Newname: lbObj.Name + "_stale"}, "rename"},
+			{netscaler.Lbvserver.Type(), lbObj.Name, lb.Lbvserver{Name: lbObj.Name + "_stale"}, "disable"}}))
+	confErr.updateError(doNitro(client, nitroConfig{netscaler.Servicegroup.Type(), lbObj.Name, nil, "delete"}, nil, nil))
+	// Get rid of HTTP-Inline lbmonitor also if present.
+	lbMonName := getLbMonName(lbObj.Name)
+	confErr.updateError(doNitro(client, nitroConfig{netscaler.Lbmonitor.Type(), lbMonName, map[string]string{"monitorname": lbMonName, "type": "HTTP-INLINE"}, "delete"}, []string{"No such resource"}, nil))
+	return confErr.getError()
+}
+
+// ServiceGroupMember is a way of specifying the ip/domain-name and port of each service endpoint associayted with an LB vserver
+type ServiceGroupMember struct {
+	IP     string
+	Domain string
+	Port   int
+}
+
+// ServiceGroupAPI specifies the ip:port members associated with an LB vserver on Citrix-ADC
+type ServiceGroupAPI struct {
+	Name    string
+	Members []ServiceGroupMember
+}
+
+// NewServiceGroupAPI returns a new ServiceGroupAPI object
+func NewServiceGroupAPI(name string) *ServiceGroupAPI {
+	svcGpObj := new(ServiceGroupAPI)
+	svcGpObj.Name = name
+	return svcGpObj
+}
+
+// Add method add/updates the servicegroup members
+func (svcGpObj *ServiceGroupAPI) Add(client *netscaler.NitroClient) error {
+	log.Printf("[TRACE] ServiceGroupAPI add: %v", svcGpObj)
+	confErr := newNitroError()
+	for _, member := range svcGpObj.Members {
+		if member.Domain != "" {
+			serverName := GetNSCompatibleName(member.Domain)
+			confErr.updateError(doNitro(client, nitroConfig{netscaler.Server.Type(), serverName, basic.Server{Name: serverName, Domain: member.Domain, State: "ENABLED"}, "add"}, []string{"Invalid value [domain, value differs from existing entity and it cant be updated.]"}, nil))
+			confErr.updateError(doNitro(client, nitroConfig{netscaler.Servicegroup_servicegroupmember_binding.Type(), svcGpObj.Name, basic.Servicegroupservicegroupmemberbinding{Servicegroupname: svcGpObj.Name, Servername: serverName, Port: member.Port}, "add"}, []string{"Resource already exists"}, nil))
+		} else {
+			confErr.updateError(doNitro(client, nitroConfig{netscaler.Servicegroup_servicegroupmember_binding.Type(), svcGpObj.Name, basic.Servicegroupservicegroupmemberbinding{Servicegroupname: svcGpObj.Name, Ip: member.IP, Port: member.Port}, "add"}, []string{"Resource already exists"}, nil))
+		}
+	}
+	/* get all bindings*/
+	svcGpBindings, err := client.FindResourceArray(netscaler.Servicegroup_servicegroupmember_binding.Type(), svcGpObj.Name)
+	if err != nil {
+		confErr.updateError(err)
+		return confErr.getError()
+	}
+	/*delete state bindings*/
+	for _, svcGpBinding := range svcGpBindings {
+		if servernameVal, ok := svcGpBinding["servername"]; ok {
+			if servername, ok := servernameVal.(string); ok {
+				if portVal, ok := svcGpBinding["port"]; ok {
+					if portF, ok := portVal.(float64); ok {
+						port := int(portF)
+						found := false
+						for _, member := range svcGpObj.Members {
+							if member.Port == port &&
+								(member.IP == servername ||
+									(member.Domain != "" && GetNSCompatibleName(member.Domain) == servername)) {
+								found = true
+								break
+							}
+						}
+						if found == false {
+							deleteBindingMap := map[string]string{"port": fmt.Sprint(port)}
+							if net.ParseIP(servername) == nil {
+								deleteBindingMap["servername"] = servername
+							} else {
+								deleteBindingMap["ip"] = servername
+							}
+							confErr.updateError(doNitro(client, nitroConfig{netscaler.Servicegroup_binding.Type(), svcGpObj.Name, deleteBindingMap, "delete"}, nil, nil))
+						}
+					}
+				}
+			}
+		}
+	}
+	return confErr.getError()
+}
---
+++ citrix-istio-adaptor/nsconfigengine/lb_api_test.go
@@ -0,0 +1,325 @@
+/*
+Copyright 2019 Citrix Systems, Inc
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+    http://www.apache.org/licenses/LICENSE-2.0
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package nsconfigengine
+
+import (
+	"citrix-istio-adaptor/tests/env"
+	"testing"
+)
+
+func Test_getLbMonName(t *testing.T) {
+	testCases := []struct {
+		input          string
+		expectedOutput string
+	}{
+		{"outbound_4040_v1_httpserverbigbigname_istio130_svc_cluster_local", "v1_httpserverbigbigname_istio130_lbmon"},
+		{"outbound_4040__httpserverbigbigname_istio130_svc_cluster_local", "_httpserverbigbigname_istio130_lbmon"},
+		{"outbound___httpserverbigbigname_istio130_svc_cluster_local", "_httpserverbigbigname_istio130_lbmon"},
+		{"outbound_4040_v1_httpserver_istio130_svc_cluster_local", "outbound_4040_v1_httpserver_istio130_svc_cluster_local_lbmon"},
+	}
+
+	for _, c := range testCases {
+		output := getLbMonName(c.input)
+		if output != c.expectedOutput {
+			t.Errorf("FAILED!!! Expected: %s. Received: %s", c.expectedOutput, output)
+		} else {
+			t.Logf("PASSED for %s\n", c.input)
+		}
+	}
+}
+
+func Test_convertTimeUnits(t *testing.T) {
+	type EI struct {
+		time int
+		unit string
+	}
+	type EO EI
+
+	testCases := []struct {
+		input          EI
+		expectedOutput EO
+	}{
+		{EI{0, "MSEC"}, EO{defaultInterval, "SEC"}},
+		{EI{10000, ""}, EO{defaultInterval, "SEC"}},
+		{EI{20000, "MSEC"}, EO{20000, "MSEC"}},
+		{EI{20940, "MSEC"}, EO{20940, "MSEC"}},
+		{EI{20941, "MSEC"}, EO{20, "SEC"}},
+		{EI{20941000, "MSEC"}, EO{349, "MIN"}},
+		{EI{1506000000, "MSEC"}, EO{20940, "MIN"}},
+		{EI{20940, "SEC"}, EO{20940, "SEC"}},
+		{EI{26000, "SEC"}, EO{433, "MIN"}},
+		{EI{2600, "SEC"}, EO{2600, "SEC"}},
+		{EI{26, "MIN"}, EO{26, "MIN"}},
+		{EI{26000, "MIN"}, EO{20940, "MIN"}},
+	}
+
+	for _, c := range testCases {
+		time, unit := convertTimeUnits(c.input.time, c.input.unit, maxInterval, defaultInterval)
+		if time != c.expectedOutput.time || unit != c.expectedOutput.unit {
+			t.Errorf("FAILED!!! Expected: %d %s. Received: %d %s", c.expectedOutput.time, c.expectedOutput.unit, time, unit)
+		} else {
+			t.Logf("PASSED for %v\n", c)
+		}
+	}
+
+}
+
+func Test_LBApi_http(t *testing.T) {
+	lbObj := NewLBApi("lbent1", "HTTP", "HTTP", "ROUNDROBIN")
+	lbObj.MaxConnections = 200
+	lbObj.MaxHTTP2ConcurrentStreams = 300
+	lbObj.MaxRequestsPerConnection = 50
+	lbObj.LbMonitorObj = new(LBMonitor)
+	lbObj.LbMonitorObj.Retries = 7
+	lbObj.LbMonitorObj.Interval = 3
+	lbObj.LbMonitorObj.IntervalUnits = "SEC"
+	lbObj.LbMonitorObj.DownTime = 10
+	lbObj.LbMonitorObj.DownTimeUnits = "SEC"
+	client := env.GetNitroClient()
+	t.Logf("Test LBApi.Add")
+	err := lbObj.Add(client)
+	if err != nil {
+		t.Errorf("LBApi add failed with %v", err)
+	}
+	configs := []env.VerifyNitroConfig{
+		{"lbvserver", "lbent1", map[string]interface{}{"name": "lbent1", "servicetype": "HTTP"}},
+		{"nshttpprofile", "nshttp_profile_300", map[string]interface{}{"name": "nshttp_profile_300", "http2": "ENABLED", "http2maxconcurrentstreams": 300}},
+		{"servicegroup", "lbent1", map[string]interface{}{"servicegroupname": "lbent1", "servicetype": "HTTP", "maxclient": 200, "maxreq": 50, "httpprofilename": "nshttp_profile_300"}},
+		{"lbvserver_servicegroup_binding", "lbent1", map[string]interface{}{"name": "lbent1", "servicegroupname": "lbent1"}},
+		{"lbmonitor", "lbent1_lbmon", map[string]interface{}{"monitorname": "lbent1_lbmon", "type": "HTTP-INLINE", "action": "DOWN", "respcode": []interface{}{"200"}, "httprequest": "HEAD /", "retries": 7, "interval": 3, "downtime": 10}},
+		{"servicegroup_lbmonitor_binding", "lbent1", map[string]interface{}{"servicegroupname": "lbent1", "monitor_name": "lbent1_lbmon"}},
+	}
+	err = env.VerifyConfigBlockPresence(client, configs)
+	if err != nil {
+		t.Errorf("Config verification failed for Add %v, error %v", "lbent1", err)
+	}
+	t.Logf("Test LBApi update")
+	lbObj.MaxConnections = 100
+	lbObj.MaxRequestsPerConnection = 0
+	lbObj.MaxHTTP2ConcurrentStreams = 0
+	lbObj.LbMonitorObj.Retries = 0
+	lbObj.LbMonitorObj.Interval = 5
+	lbObj.LbMonitorObj.IntervalUnits = "SEC"
+	lbObj.LbMonitorObj.DownTime = 0
+	err = lbObj.Add(client)
+	if err != nil {
+		t.Errorf("LBApi add failed with %v", err)
+	}
+	configs = []env.VerifyNitroConfig{
+		{"lbvserver", "lbent1", map[string]interface{}{"name": "lbent1", "servicetype": "HTTP"}},
+		{"servicegroup", "lbent1", map[string]interface{}{"servicegroupname": "lbent1", "servicetype": "HTTP", "maxclient": 100, "maxreq": 0}},
+		{"lbvserver_servicegroup_binding", "lbent1", map[string]interface{}{"name": "lbent1", "servicegroupname": "lbent1"}},
+		{"lbmonitor", "lbent1_lbmon", map[string]interface{}{"monitorname": "lbent1_lbmon", "type": "HTTP-INLINE", "action": "DOWN", "respcode": []interface{}{"200"}, "httprequest": "HEAD /", "retries": defaultRetries, "interval": 5, "downtime": defaultDownTime}},
+	}
+	err = env.VerifyConfigBlockPresence(client, configs)
+	if err != nil {
+		t.Errorf("Config verification failed for Update %v, error %v", "lbent1", err)
+	}
+
+	t.Logf("Test LBApi update with LB Monitor removal")
+	lbObj.LbMonitorObj = nil
+	lbObj.MaxConnections = 100
+	lbObj.MaxRequestsPerConnection = 0
+	lbObj.MaxHTTP2ConcurrentStreams = 0
+	err = lbObj.Add(client)
+	if err != nil {
+		t.Errorf("LBApi add failed with %v", err)
+	}
+	configs = []env.VerifyNitroConfig{
+		{"lbvserver", "lbent1", map[string]interface{}{"name": "lbent1", "servicetype": "HTTP"}},
+		{"servicegroup", "lbent1", map[string]interface{}{"servicegroupname": "lbent1", "servicetype": "HTTP", "maxclient": 100, "maxreq": 0}},
+		{"lbvserver_servicegroup_binding", "lbent1", map[string]interface{}{"name": "lbent1", "servicegroupname": "lbent1"}},
+	}
+	err = env.VerifyConfigBlockPresence(client, configs)
+	if err != nil {
+		t.Errorf("Config verification failed for Update with LB Monitor removal %v, error %v", "lbent1", err)
+	}
+	t.Logf("Test LBApi delete")
+	err = lbObj.Delete(client)
+	if err != nil {
+		t.Errorf("LBApi delete failed with %v", err)
+	}
+	configs = []env.VerifyNitroConfig{
+		{"lbvserver", "lbent1", map[string]interface{}{"name": "lbent1", "servicetype": "HTTP"}},
+		{"servicegroup", "lbent1", map[string]interface{}{"servicegroupname": "lbent1", "servicetype": "HTTP", "maxclient": 100, "maxreq": 0, "httpprofilename": "http_default_profile"}},
+		{"lbmonitor", "lbent1_lbmon", map[string]interface{}{"monitorname": "lbent1_lbmon", "type": "HTTP-INLINE", "action": "DOWN", "respcode": []int{200}, "httprequest": "HEAD /", "retries": 5, "interval": 5, "downtime": 10}},
+	}
+	err = env.VerifyConfigBlockAbsence(client, configs)
+	if err != nil {
+		t.Errorf("Config verification failed for Delete %v, error %v", "lbent1", err)
+	}
+}
+
+func Test_LBApi_tcp(t *testing.T) {
+	lbObj := NewLBApi("lbent2", "TCP", "TCP", "LEASTCONNECTION")
+	lbObj.MaxConnections = 100
+	lbObj.MaxRequestsPerConnection = 25
+	client := env.GetNitroClient()
+	t.Logf("Test LBApi.Add")
+	err := lbObj.Add(client)
+	if err != nil {
+		t.Errorf("LBApi add failed with %v", err)
+	}
+	configs := []env.VerifyNitroConfig{
+		{"lbvserver", "lbent2", map[string]interface{}{"name": "lbent2", "servicetype": "TCP"}},
+		{"servicegroup", "lbent2", map[string]interface{}{"servicegroupname": "lbent2", "servicetype": "TCP", "maxclient": 100, "maxreq": 25}},
+		{"lbvserver_servicegroup_binding", "lbent2", map[string]interface{}{"name": "lbent2", "servicegroupname": "lbent2"}},
+	}
+	err = env.VerifyConfigBlockPresence(client, configs)
+	if err != nil {
+		t.Errorf("Config verification failed for Add %v, error %v", "lbent2", err)
+	}
+	t.Logf("Test LBApi update")
+	lbObj.MaxConnections = 0
+	lbObj.MaxRequestsPerConnection = 100
+	lbObj.MaxHTTP2ConcurrentStreams = 0
+	err = lbObj.Add(client)
+	if err != nil {
+		t.Errorf("LBApi add failed with %v", err)
+	}
+	configs = []env.VerifyNitroConfig{
+		{"lbvserver", "lbent2", map[string]interface{}{"name": "lbent2", "servicetype": "TCP"}},
+		{"servicegroup", "lbent2", map[string]interface{}{"servicegroupname": "lbent2", "servicetype": "TCP", "maxclient": 0, "maxreq": 100}},
+		{"lbvserver_servicegroup_binding", "lbent2", map[string]interface{}{"name": "lbent2", "servicegroupname": "lbent2"}},
+	}
+	err = env.VerifyConfigBlockPresence(client, configs)
+	if err != nil {
+		t.Errorf("Config verification failed for Update %v, error %v", "lbent1", err)
+	}
+	t.Logf("Test LBApi delete")
+	err = lbObj.Delete(client)
+	if err != nil {
+		t.Errorf("LBApi delete failed with %v", err)
+	}
+	configs = []env.VerifyNitroConfig{
+		{"lbvserver", "lbent2", map[string]interface{}{"name": "lbent2", "servicetype": "TCP"}},
+		{"servicegroup", "lbent2", map[string]interface{}{"servicegroupname": "lbent2", "servicetype": "TCP"}},
+	}
+	err = env.VerifyConfigBlockAbsence(client, configs)
+	if err != nil {
+		t.Errorf("Config verification failed for Delete %v, error %v", "lbent2", err)
+	}
+}
+
+func Test_LBApi_http_tls(t *testing.T) {
+	lbObj := NewLBApi("lbent1s", "HTTP", "SSL", "ROUNDROBIN")
+	lbObj.MaxConnections = 200
+	lbObj.MaxHTTP2ConcurrentStreams = 300
+	lbObj.MaxRequestsPerConnection = 50
+	lbObj.LbMonitorObj = new(LBMonitor)
+	lbObj.LbMonitorObj.Retries = 7
+	lbObj.LbMonitorObj.Interval = 3
+	lbObj.LbMonitorObj.IntervalUnits = "SEC"
+	lbObj.LbMonitorObj.DownTime = 10
+	lbObj.LbMonitorObj.DownTimeUnits = "SEC"
+	lbObj.BackendTLS = []SSLSpec{{CertFilename: "../tests/certs/certssvc1/svc1.citrixrootdummy1.com.crt", PrivateKeyFilename: "../tests/certs/certssvc1/svc1.citrixrootdummy1.com.key", RootCertFilename: "../tests/certs/certssvc1/rootCA.crt"}}
+	client := env.GetNitroClient()
+	UploadCert(client, "../tests/certs/certssvc1/svc1.citrixrootdummy1.com.crt", "certssvc1_svc1", "../tests/certs/certssvc1/svc1.citrixrootdummy1.com.key", "certssvc1_svc1_key")
+	UploadCert(client, "../tests/certs/certssvc1/rootCA.crt", "certssvc1_rootCA", "", "")
+	t.Logf("Test LBApi.Add")
+	err := lbObj.Add(client)
+	if err != nil {
+		t.Errorf("LBApi add failed with %v", err)
+	}
+	configs := []env.VerifyNitroConfig{
+		{"lbvserver", "lbent1s", map[string]interface{}{"name": "lbent1s", "servicetype": "HTTP"}},
+		{"nshttpprofile", "nshttp_profile_300", map[string]interface{}{"name": "nshttp_profile_300", "http2": "ENABLED", "http2maxconcurrentstreams": 300}},
+		{"servicegroup", "lbent1s", map[string]interface{}{"servicegroupname": "lbent1s", "servicetype": "SSL", "maxclient": 200, "maxreq": 50}},
+		{"lbvserver_servicegroup_binding", "lbent1s", map[string]interface{}{"name": "lbent1s", "servicegroupname": "lbent1s"}},
+		{"lbmonitor", "lbent1s_lbmon", map[string]interface{}{"monitorname": "lbent1s_lbmon", "type": "HTTP-INLINE", "action": "DOWN", "respcode": []interface{}{"200"}, "httprequest": "HEAD /", "retries": 7, "interval": 3, "downtime": 10}},
+		{"servicegroup_lbmonitor_binding", "lbent1s", map[string]interface{}{"servicegroupname": "lbent1s", "monitor_name": "lbent1s_lbmon"}},
+		{"sslcertkey", "certssvc1_svc1", map[string]interface{}{"cert": "/nsconfig/ssl/certssvc1_svc1", "certkey": "certssvc1_svc1", "key": "/nsconfig/ssl/certssvc1_svc1_key"}},
+		{"sslcertkey", "certssvc1_rootCA", map[string]interface{}{"cert": "/nsconfig/ssl/certssvc1_rootCA", "certkey": "certssvc1_rootCA"}},
+		{"sslservicegroup", "lbent1s", map[string]interface{}{"serverauth": "ENABLED", "servicegroupname": "lbent1s"}},
+	}
+	err = env.VerifyConfigBlockPresence(client, configs)
+	if err != nil {
+		t.Errorf("Config verification failed for Add %v, error %v", "lbent1s", err)
+	}
+	err = env.VerifyBindings(client, "sslservicegroup", "lbent1s", "sslcertkey", []map[string]interface{}{{"ca": true, "certkeyname": "certssvc1_rootCA", "servicegroupname": "lbent1s"}, {"certkeyname": "certssvc1_svc1", "servicegroupname": "lbent1s"}})
+	if err != nil {
+		t.Errorf("Config verification failed for Add binding %v, error %v", "lbent1s", err)
+	}
+	t.Logf("Test LBApi delete")
+	err = lbObj.Delete(client)
+	if err != nil {
+		t.Errorf("LBApi delete failed with %v", err)
+	}
+	configs = []env.VerifyNitroConfig{
+		{"lbvserver", "lbent1s", map[string]interface{}{"name": "lbent1s", "servicetype": "SSL"}},
+		{"servicegroup", "lbent1s", map[string]interface{}{"servicegroupname": "lbent1s", "servicetype": "HTTP"}},
+		{"lbmonitor", "lbent1s_lbmon", map[string]interface{}{"monitorname": "lbent1s_lbmon", "type": "HTTP-INLINE", "action": "DOWN", "respcode": []int{200}, "httprequest": "HEAD /", "retries": 7, "interval": 3, "downtime": 10}},
+	}
+	err = env.VerifyConfigBlockAbsence(client, configs)
+	if err != nil {
+		t.Errorf("Config verification failed for Delete %v, error %v", "lbent1s", err)
+	}
+}
+
+func Test_ServiceGroupAPI_ip(t *testing.T) {
+	client := env.GetNitroClient()
+	client.AddResource("servicegroup", "svcgp1", map[string]interface{}{"servicegroupname": "svcgp1", "servicetype": "HTTP"})
+	svcGpObj := NewServiceGroupAPI("svcgp1")
+
+	svcGpObj.Members = []ServiceGroupMember{{IP: "1.1.1.1", Port: 80}, {IP: "2.2.2.2", Port: 9090}, {IP: "1.1.1.2", Port: 80}}
+	err := svcGpObj.Add(client)
+	if err != nil {
+		t.Errorf("ServiceGroup members add failed with %v", err)
+	}
+	err = env.VerifyBindings(client, "servicegroup", "svcgp1", "servicegroupmember", []map[string]interface{}{{"ip": "1.1.1.1", "port": 80}, {"ip": "2.2.2.2", "port": 9090}, {"ip": "1.1.1.2", "port": 80}})
+	if err != nil {
+		t.Errorf("Config verification for add failed with error %v", err)
+	}
+
+	svcGpObj.Members = []ServiceGroupMember{{IP: "1.1.1.2", Port: 80}, {IP: "3.3.3.3", Port: 80}, {IP: "2.2.2.2", Port: 9090}}
+	err = svcGpObj.Add(client)
+	if err != nil {
+		t.Errorf("ServiceGroup members update failed with %v", err)
+	}
+	err = env.VerifyBindings(client, "servicegroup", "svcgp1", "servicegroupmember", []map[string]interface{}{{"ip": "3.3.3.3", "port": 80}, {"ip": "2.2.2.2", "port": 9090}, {"ip": "1.1.1.2", "port": 80}})
+	if err != nil {
+		t.Errorf("Config verification for update failed with error %v", err)
+	}
+	client.DeleteResource("servicegroup", "svcgp1")
+}
+
+func Test_ServiceGroupAPI_domain(t *testing.T) {
+	client := env.GetNitroClient()
+	client.AddResource("servicegroup", "svcgp2", map[string]interface{}{"servicegroupname": "svcgp2", "servicetype": "HTTP"})
+	svcGpObj := NewServiceGroupAPI("svcgp2")
+
+	svcGpObj.Members = []ServiceGroupMember{{Domain: "www.google.com", Port: 80}, {Domain: "www.abc.org", Port: 9090}, {Domain: "www.facebook.com", Port: 80}}
+	err := svcGpObj.Add(client)
+	if err != nil {
+		t.Errorf("ServiceGroup members add failed with %v", err)
+		return
+	}
+	err = env.VerifyBindings(client, "servicegroup", "svcgp2", "servicegroupmember", []map[string]interface{}{{"servername": "www_google_com", "port": 80}, {"servername": "www_abc_org", "port": 9090}, {"servername": "www_facebook_com", "port": 80}})
+	if err != nil {
+		t.Errorf("Config verification for add failed with error %v", err)
+		return
+	}
+
+	svcGpObj.Members = []ServiceGroupMember{{Domain: "www.googl1.com", Port: 8080}, {Domain: "www.abc.org", Port: 9090}, {Domain: "citrix.com", Port: 80}}
+	err = svcGpObj.Add(client)
+	if err != nil {
+		t.Errorf("ServiceGroup members update failed with %v", err)
+		return
+	}
+	err = env.VerifyBindings(client, "servicegroup", "svcgp2", "servicegroupmember", []map[string]interface{}{{"servername": "www_googl1_com", "port": 8080}, {"servername": "www_abc_org", "port": 9090}, {"servername": "citrix_com", "port": 80}})
+	if err != nil {
+		t.Errorf("Config verification for update failed with error %v", err)
+	}
+	client.DeleteResource("servicegroup", "svcgp2")
+}
